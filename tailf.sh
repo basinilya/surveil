49ec7d5a4c300db9333efac4f5d922acab70dc09
Ilya Basin <basinilya@gmail.com>
2015-09-30 19:15:56 +0300

    x

--- tailf.sh
+++ tailf.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-echo "connection received1"
+exec 4>&1
+exec 1> >(exec logger -t tailf.sh >/dev/null 2>&1 4>&- ) 2>&1
+THISDIR=`cd "\`dirname \"$0\"\`" && pwd`
+exec -- "$THISDIR/fm" repl_getclip_backend
