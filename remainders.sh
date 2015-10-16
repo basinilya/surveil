d94c24b3033c82e8a62897671ffb08ebbcb3bd9f
Ilya Basin <basinilya@gmail.com>
2015-10-16 11:40:42 +0300

    x

--- remainders.sh
+++ remainders.sh
@@ -4,7 +4,7 @@ clean=( /home/il/surveil/cleandisk --dir /var/cache/surveil/cam --minfree $((90*
 "${clean[@]}"
 while true; do
     rc=0
-    rsync -rv --append $(for i in +1 +0 -1; do echo --exclude=`date -d "${i}hour" +/\%Y\%m\%d/"*"-\%H-"??-??.*"`; done) \
+    rsync -rv --times --append $(for i in +1 +0 -1; do echo --exclude=`date -d "${i}hour" +/\%Y\%m\%d/"*"-\%H-"??-??.*"`; done) \
         rsync://localhost:10873/surveillance/cam/ /var/cache/surveil/cam/ || rc=$?
     echo rc=$rc
     "${clean[@]}"
